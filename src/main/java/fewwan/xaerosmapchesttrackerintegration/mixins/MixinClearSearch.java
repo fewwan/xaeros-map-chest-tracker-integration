f975a7ecc3176326d849107ffb0bc26ebb1b0b81
Fewwan <ferranvillpov@gmail.com>
2023-08-12 16:57:49 +0200

    Updated license headers

--- src/main/java/fewwan/xaerosmapchesttrackerintegration/mixins/MixinClearSearch.java
+++ src/main/java/fewwan/xaerosmapchesttrackerintegration/mixins/MixinClearSearch.java
@@ -1,21 +1,21 @@
 /*
- * This file is part of the "Xaero's Map Chest Tracker Integration" project, licensed under the
+ * This file is part of the Xaero's Map Chest Tracker Integration project, licensed under the
  * GNU Lesser General Public License v3.0
  *
- * Copyright (C) 2023  Fallen_Breath and contributors
+ * Copyright (C) 2023  Fewwan and contributors
  *
- * "Xaero's Map Chest Tracker Integration" is free software: you can redistribute it and/or modify
+ * Xaero's Map Chest Tracker Integration is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * "Xaero's Map Chest Tracker Integration" is distributed in the hope that it will be useful,
+ * Xaero's Map Chest Tracker Integration is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with "Xaero's Map Chest Tracker Integration".  If not, see <https://www.gnu.org/licenses/>.
+ * along with Xaero's Map Chest Tracker Integration.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 package fewwan.xaerosmapchesttrackerintegration.mixins;
