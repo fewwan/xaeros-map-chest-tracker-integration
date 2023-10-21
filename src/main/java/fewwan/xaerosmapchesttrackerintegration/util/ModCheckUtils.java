60cb387a2e5480cbe0755f5dec4a6642376fe7d8
Fewwan <ferranvillpov@gmail.com>
2023-10-21 16:54:44 +0200

    Added version checking for Xaero's minimap

--- src/main/java/fewwan/xaerosmapchesttrackerintegration/util/ModCheckUtils.java
+++ src/main/java/fewwan/xaerosmapchesttrackerintegration/util/ModCheckUtils.java
@@ -21,17 +21,52 @@
 package fewwan.xaerosmapchesttrackerintegration.util;
 
 import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.ModContainer;
+import net.fabricmc.loader.api.Version;
+import net.fabricmc.loader.api.VersionParsingException;
+import net.fabricmc.loader.api.metadata.CustomValue;
+import net.fabricmc.loader.api.metadata.version.VersionPredicate;
+import net.fabricmc.loader.impl.util.version.VersionPredicateParser;
+
+import java.util.Map;
+import java.util.Optional;
+
+import static fewwan.xaerosmapchesttrackerintegration.XaerosMapChestTrackerIntegration.MOD_ID;
+
 
 public class ModCheckUtils {
-    public static boolean isXaeroModLoaded() {
-        String[] validMods = {"xaerominimap", "xaerominimapfair", "xaerobetterpvp", "xaerobetterpvpfair"};
+    public static boolean isXaeroMinimapModLoaded() {
+        FabricLoader loader = FabricLoader.getInstance();
+        ModContainer mod = loader.getModContainer(MOD_ID).orElseThrow(NullPointerException::new);
+        CustomValue.CvObject validXaerosMinimap = mod.getMetadata().getCustomValue(MOD_ID).getAsObject().get("validXaerosMinimap").getAsObject();
 
-        for (String modId : validMods) {
-            if (FabricLoader.getInstance().isModLoaded(modId)) {
-                return true;
+        for (Map.Entry<String, CustomValue> entry : validXaerosMinimap) {
+            CustomValue value = entry.getValue();
+            String modId = entry.getKey();
+
+            // means that there is not a compatible version yet
+            if (value.getType() == CustomValue.CvType.NULL) continue;
+
+            String versionConstraintString = value.getAsString();
+
+            // means that there is not a compatible version yet
+            if (versionConstraintString.equals("none")) continue;
+
+            VersionPredicate versionConstraint;
+            try {
+                versionConstraint = VersionPredicateParser.parse(versionConstraintString);
+            } catch (VersionParsingException e) {
+                throw new RuntimeException(e);
+            }
+
+            Optional<ModContainer> minimapMod = loader.getModContainer(modId);
+            if (minimapMod.isPresent()) {
+                ModContainer minimapModContainer = minimapMod.get();
+                Version minimapVersion = minimapModContainer.getMetadata().getVersion();
+                return versionConstraint.test(minimapVersion);
             }
         }
+
         return false;
     }
-
 }
