bced9b8b6f6502a3f53e1a6f61f97a0f974746a9
Fewwan <ferranvillpov@gmail.com>
2024-08-26 15:48:04 +0200

    Improved recursive search

--- src/client/java/fewwan/xaerosmapchesttrackerintegration/plugin/WhereIsItXaerosMapChestTrakerIntegrationPlugin.java
+++ src/client/java/fewwan/xaerosmapchesttrackerintegration/plugin/WhereIsItXaerosMapChestTrakerIntegrationPlugin.java
@@ -3,6 +3,10 @@ package fewwan.xaerosmapchesttrackerintegration.plugin;
 import fewwan.xaerosmapchesttrackerintegration.util.CountUtils;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtElement;
+import net.minecraft.nbt.NbtList;
+import net.minecraft.registry.Registries;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
@@ -10,10 +14,12 @@ import red.jackf.chesttracker.api.memory.Memory;
 import red.jackf.chesttracker.api.memory.MemoryBank;
 import red.jackf.chesttracker.api.providers.ProviderUtils;
 import red.jackf.chesttracker.impl.memory.MemoryBankAccessImpl;
+import red.jackf.whereisit.api.SearchRequest;
 import red.jackf.whereisit.api.SearchResult;
 import red.jackf.whereisit.client.api.WhereIsItClientPlugin;
 import red.jackf.whereisit.client.api.events.OnResult;
 import red.jackf.whereisit.client.api.events.OnResultsCleared;
+import red.jackf.whereisit.client.api.events.SearchInvoker;
 import xaero.common.XaeroMinimapSession;
 import xaero.common.minimap.waypoints.Waypoint;
 import xaero.common.minimap.waypoints.WaypointSet;
@@ -22,13 +28,48 @@ import xaero.common.minimap.waypoints.WaypointsManager;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Consumer;
 
 public class WhereIsItXaerosMapChestTrakerIntegrationPlugin implements WhereIsItClientPlugin {
     private static final String SUFFIX = " [CT]";
     private static final int COLOR_ID = 0;
     private static boolean running = false;
+    private static Item searchItem = null;
+
+    private static Item getItemFromSearchRequest(SearchRequest request) {
+        NbtCompound packedRequest = request.pack();
+
+        if (packedRequest.contains(SearchRequest.DATA, NbtElement.LIST_TYPE)) {
+            NbtList criterionList = packedRequest.getList(SearchRequest.DATA, NbtElement.COMPOUND_TYPE);
+            for (NbtElement element : criterionList) {
+                if (element instanceof NbtCompound compound) {
+                    if (compound.contains(SearchRequest.ID, NbtElement.STRING_TYPE) &&
+                            "whereisit:item".equals(compound.getString("Id"))) {
+                        if (compound.contains(SearchRequest.DATA, NbtElement.COMPOUND_TYPE)) {
+                            NbtCompound data = compound.getCompound(SearchRequest.DATA);
+                            if (data.contains("ItemId", NbtElement.STRING_TYPE)) {
+                                String itemId = data.getString("ItemId");
+                                Identifier itemIdentifier = Identifier.tryParse(itemId);
+                                if (itemIdentifier != null) {
+                                    return Registries.ITEM.get(itemIdentifier);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return null;
+    }
+
+    public static boolean doSearch(SearchRequest request, Consumer<Collection<SearchResult>> collectionConsumer) {
+        searchItem = getItemFromSearchRequest(request);
+        return true;
+    }
 
     private static void onResults(Collection<SearchResult> results) {
+        if (searchItem == null) return;
         running = true;
 
         XaeroMinimapSession minimapSession = XaeroMinimapSession.getCurrentSession();
@@ -43,13 +84,10 @@ public class WhereIsItXaerosMapChestTrakerIntegrationPlugin implements WhereIsIt
         Optional<Identifier> key = ProviderUtils.getPlayersCurrentKey();
         Optional<MemoryBank> optionalMemoryBank = MemoryBankAccessImpl.INSTANCE.getLoaded();
         if (optionalMemoryBank.isEmpty()) return;
-
         MemoryBank memoryBank = optionalMemoryBank.get();
 
         for (SearchResult result : results) {
             BlockPos pos = result.pos();
-            ItemStack searchStack = result.item();
-            Item searchItem = searchStack.getItem();
 
             Optional<Memory> optionalMemory = memoryBank.getMemory(key, pos);
             if (optionalMemory.isEmpty()) continue;
@@ -57,7 +95,7 @@ public class WhereIsItXaerosMapChestTrakerIntegrationPlugin implements WhereIsIt
             Memory memory = optionalMemory.get();
             int totalItemCount = CountUtils.countItemsOf(memory.items(), searchItem);
 
-            String itemName = Text.translatable(searchStack.getTranslationKey()).getString();
+            String itemName = Text.translatable(searchItem.getTranslationKey()).getString();
             String waypointName = totalItemCount + " " + itemName + SUFFIX;
             String waypointLabel = totalItemCount <= 99 ? String.valueOf(totalItemCount) : "99";
 
@@ -81,8 +119,10 @@ public class WhereIsItXaerosMapChestTrakerIntegrationPlugin implements WhereIsIt
         waypoints.removeIf(waypoint -> waypoint.isTemporary() && waypoint.getName().endsWith(SUFFIX));
     }
 
+
     @Override
     public void load() {
+        SearchInvoker.EVENT.register(WhereIsItXaerosMapChestTrakerIntegrationPlugin::doSearch);
         OnResult.EVENT.register(WhereIsItXaerosMapChestTrakerIntegrationPlugin::onResults);
         OnResultsCleared.EVENT.register(WhereIsItXaerosMapChestTrakerIntegrationPlugin::onResultsCleared);
     }
--- src/client/java/fewwan/xaerosmapchesttrackerintegration/util/CountUtils.java
+++ src/client/java/fewwan/xaerosmapchesttrackerintegration/util/CountUtils.java
@@ -2,40 +2,18 @@ package fewwan.xaerosmapchesttrackerintegration.util;
 
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtElement;
 import net.minecraft.nbt.NbtList;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 public class CountUtils {
-    static final Set<Item> shulkerBoxes = Set.of(
-            Items.SHULKER_BOX,
-            Items.WHITE_SHULKER_BOX,
-            Items.LIGHT_GRAY_SHULKER_BOX,
-            Items.GRAY_SHULKER_BOX,
-            Items.BLACK_SHULKER_BOX,
-            Items.RED_SHULKER_BOX,
-            Items.ORANGE_SHULKER_BOX,
-            Items.YELLOW_SHULKER_BOX,
-            Items.LIME_SHULKER_BOX,
-            Items.GREEN_SHULKER_BOX,
-            Items.CYAN_SHULKER_BOX,
-            Items.LIGHT_BLUE_SHULKER_BOX,
-            Items.BLUE_SHULKER_BOX,
-            Items.PURPLE_SHULKER_BOX,
-            Items.MAGENTA_SHULKER_BOX,
-            Items.PINK_SHULKER_BOX,
-            Items.BROWN_SHULKER_BOX
-    );
-
-    public static List<ItemStack> getItemsInsideShulkerBox(ItemStack shulkerBox) {
+    public static List<ItemStack> getItemsInsideContainer(ItemStack containerStack) {
         List<ItemStack> itemsInside = new ArrayList<>();
 
-        NbtCompound shulkerBoxNbtCompound = shulkerBox.getNbt();
+        NbtCompound shulkerBoxNbtCompound = containerStack.getNbt();
         if (shulkerBoxNbtCompound != null && shulkerBoxNbtCompound.contains("BlockEntityTag", NbtElement.COMPOUND_TYPE)) {
             NbtCompound blockEntityTag = shulkerBoxNbtCompound.getCompound("BlockEntityTag");
             if (blockEntityTag.contains("Items", NbtElement.LIST_TYPE)) {
@@ -56,9 +34,8 @@ public class CountUtils {
         for (ItemStack stack : items) {
             if (stack.getItem() == searchItem) {
                 itemCount += stack.getCount();
-            } else if (shulkerBoxes.contains(stack.getItem())) {
-                itemCount += countItemsOf(getItemsInsideShulkerBox(stack), searchItem) * stack.getCount();
             }
+            itemCount += countItemsOf(getItemsInsideContainer(stack), searchItem) * stack.getCount();
         }
         return itemCount;
     }
