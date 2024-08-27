0d4fa5ac52caa82326ced7d11e4c52b5d0f5d406
Fewwan <ferranvillpov@gmail.com>
2024-08-28 01:12:25 +0200

    Reformat Code

--- src/client/java/fewwan/xaerosmapchesttrackerintegration/XaerosMapChestTrackerIntegrationClient.java
+++ src/client/java/fewwan/xaerosmapchesttrackerintegration/XaerosMapChestTrackerIntegrationClient.java
@@ -6,13 +6,13 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class XaerosMapChestTrackerIntegrationClient implements ClientModInitializer {
-	public static final String MOD_ID = "xaeros-map-chest-tracker-integration";
-	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
+    public static final String MOD_ID = "xaeros-map-chest-tracker-integration";
+    public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
-	@Override
-	public void onInitializeClient() {
-		if (!ModCheckUtils.isXaeroMinimapModLoaded()) {
-			LOGGER.error("Xaero's Minimap mod not found. Please install it to use this mod.");
-		}
-	}
+    @Override
+    public void onInitializeClient() {
+        if (!ModCheckUtils.isXaeroMinimapModLoaded()) {
+            LOGGER.error("Xaero's Minimap mod not found. Please install it to use this mod.");
+        }
+    }
 }
